85e0223ede9796c97f02419260a4939481d17ba8
Johana <johana.aguero.serrano@est.una.ac.cr>
2026-04-23 18:57:04 -0600

    create base components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>CarParts App</h2>
+      <a href="/">Home</a>
+      <a href="/carparts">Repuestos</a>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/CarParts.jsx
@@ -0,0 +1,10 @@
+function CarParts() {
+  return (
+    <div>
+      <h1>Repuestos</h1>
+      <p>Aquí aparecerán los datos del API</p>
+    </div>
+  );
+}
+
+export default CarParts;
